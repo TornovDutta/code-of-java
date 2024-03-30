60724a9f7778d4a54ab5847e0e0a781d0be1b842
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-03-30 21:56:16 +0530

    Add files via upload

---
+++ soring/Quicksort.java
@@ -0,0 +1,41 @@
+package soring;
+
+import java.util.Arrays;
+
+public class Quicksort {
+    public static void main(String[] args) {
+        int arr[]={5,4,3,2,1};
+        sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void sort(int arr[],int low,int high){
+        if(low>=high){
+            return;
+        }
+        int start=low;
+        int end=high;
+        int mid=start+(end-start)/2;
+        int pivot=arr[mid];
+
+        while(start<=end){
+            while(arr[start]<pivot){
+                start++;
+            }
+
+            while(arr[end]>pivot){
+                end--;
+            }
+
+            if(start<=end){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
+        }
+
+        sort(arr,low,end);
+        sort(arr,start,high);
+    }
+}
