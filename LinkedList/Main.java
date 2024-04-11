be3addb67f50eec18ed53ad6738ec1396f6b1689
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-04-11 22:04:25 +0530

    Add files via upload

---
+++ LinkedList/Main.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insterfirst(1);
+        list.insterfirst(3);
+        list.insterfirst(2);
+
+        list.display();
+
+        list.displaySize();
+
+    }
+}
