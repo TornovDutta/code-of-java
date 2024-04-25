3da6e5b24030d4a35396a422f5d9e1cde43e6efb
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-04-25 21:10:51 +0530

    Add files via upload

---
+++ functions and methods/info.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class info {
+    public static void main(String[] args) {
+        sum();
+    }
+
+     static void sum() {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+         System.out.println(a+b);
+    }
+}
