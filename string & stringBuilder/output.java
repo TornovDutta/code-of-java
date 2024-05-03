4f0bad56977c0f113472018e246fa7ca272829d9
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-05-03 21:45:41 +0530

    Add files via upload

---
+++ string & stringBuilder/output.java
@@ -0,0 +1,7 @@
+public class output {
+    public static void main(String[] args) {
+        String name="tornov";
+        System.out.println(name);
+        System.out.println(56);
+    }
+}
---
+++ string & stringBuilder/paildrom.java
@@ -0,0 +1,20 @@
+public class paildrom {
+    public static void main(String[] args) {
+        String str="abcdcba";
+        System.out.println(pail(str));
+    }
+    static  boolean pail(String str){
+        if(str==null ||str.length()==0){
+            return true;
+        }
+        for (int i = 0; i < str.length()/2; i++) {
+            char start=str.charAt(i);
+            char end=str.charAt(str.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
